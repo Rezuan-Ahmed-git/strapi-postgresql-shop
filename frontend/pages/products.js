f63cf93ef0a74ac91e2793425c1d8d7e36246d21
Rezuan Ahmed <rezuan.ahmed.pro@gmail.com>
2022-11-23 18:04:59 +0600

    feat: single product page create

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Products = (props) => {
   return (
     <div className="container mx-auto px-4">
       <section className="text-gray-600 body-font">
-        <div className="container px-5 py-24 mx-auto">
-          <div className="flex flex-wrap w-full mb-20">
+        <div className="container px-5 md:py-24 mx-auto">
+          <div className="flex flex-wrap w-full md:mb-20">
             <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
               <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900">
                 Products - MyShop
@@ -19,7 +20,7 @@ const Products = (props) => {
                 <div className="xl:w-1/4 md:w-1/2 p-4" key={item.id}>
                   <div className="bg-gray-100 p-6 rounded-lg">
                     <img
-                      className="h-40 rounded w-full object-cover object-center mb-6"
+                      className="h-60 rounded object-cover m-auto mb-9"
                       src={item?.attributes?.image?.data?.attributes?.url}
                       alt="content"
                     />
@@ -29,9 +30,22 @@ const Products = (props) => {
                     <h2 className="text-lg text-gray-900 font-medium title-font mb-4">
                       {item.attributes.title}
                     </h2>
+                    <div className="hidden bg-yellow-800 bg-blue-800 bg-green-800"></div>
+                    <button
+                      className={
+                        'border-2 border-gray-300 ml-1 rounded-full w-6 h-6 focus:outline-none ' +
+                        `bg-${item?.attributes?.color}-800`
+                      }
+                    ></button>
+
                     <p className="leading-relaxed text-base">
                       {item.attributes.description}
                     </p>
+                    <Link href={`/product/${item.attributes.slug}`}>
+                      <button className=" my-2 text-white bg-indigo-500 border-0 py-1 md:py-2 px-4 focus:outline-none hover:bg-indigo-600 rounded text-sm">
+                        Buy Now
+                      </button>
+                    </Link>
                   </div>
                 </div>
               );
@@ -52,7 +66,7 @@ export async function getServerSideProps(context) {
     headers: headers,
   });
   let products = await a.json();
-  console.log(products);
+  //console.log(products);
   return {
     props: { products: products }, // will be passed to the page component as props
   };
